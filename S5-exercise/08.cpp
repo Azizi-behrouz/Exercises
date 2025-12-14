88ff13d87f1b2609cd7a4fb3f9a5f6e54a8e6368
Behrouz <azizi.behrouz.2021@gmail.com>
2025-12-14 14:45:57 +0330

    Create 08.cpp

---
+++ S5-exercise/08.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+float average(float a,float b,float c,float d){
+    return (a+b+c+d)/4;
+}
+
+int main(){
+    cout<<average(2,4,6,8);
+    return 0;
+}
