328870f1f425eba7078b3b882406648cc3a7a95d
Behrouz <azizi.behrouz.2021@gmail.com>
2025-12-14 14:14:07 +0330

    Create 05.cpp

---
+++ S4-exercises/05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cin >> n;
+
+    int i = 1;
+    while (i <= n) {
+        sum += i * i;
+        i++;
+    }
+
+    cout << sum;
+    return 0;
+}
