c670ff73b9cafa903d727dee3a5e57123a26eaa7
Behrouz <azizi.behrouz.2021@gmail.com>
2025-12-14 14:10:38 +0330

    Update and rename 1.cpp to 01.cpp

---
+++ S4-exercises/01.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float x = 4.15;
+    for (int i = 0; i < 3; i++)
+        x *= 2;
+
+    cout << x << endl;  // خروجی: 33.2
+    return 0;
+}
