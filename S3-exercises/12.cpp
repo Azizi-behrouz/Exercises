c2520f7fbebfe81339b4c2408636a31ff2789396
Behrouz <azizi.behrouz.2021@gmail.com>
2025-12-14 14:02:24 +0330

    Update and rename 11.cpp to 12.cpp

---
+++ S3-exercises/12.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    double d = b*b - 4*a*c;
+    double x1 = (-b + sqrt(d)) / (2*a);
+    double x2 = (-b - sqrt(d)) / (2*a);
+
+    cout << "x1 = " << x1 << endl;
+    cout << "x2 = " << x2 << endl;
+    return 0;
+}
