ccbff2e523bd0b069293b6d27787151dcf8f776a
Behrouz <azizi.behrouz.2021@gmail.com>
2025-12-14 14:15:57 +0330

    Create 11.cpp

---
+++ S4-exercises/11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    while (n != 0) {
+        int r = m % n;
+        m = n;
+        n = r;
+    }
+
+    cout << m;
+    return 0;
+}
